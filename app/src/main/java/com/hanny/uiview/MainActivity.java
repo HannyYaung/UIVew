69e8e37c64bf55e782a05bd08044189b5703037e
HannyYaung <root997@163.com>
2018-07-04 11:36:58 +0800

    自定义view

--- app/src/main/java/com/hanny/uiview/MainActivity.java
+++ app/src/main/java/com/hanny/uiview/MainActivity.java
@@ -3,6 +3,8 @@ package com.hanny.uiview;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import com.hanny.uiview.R;
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
--- app/src/main/java/com/hanny/uiview/TextView.java
+++ app/src/main/java/com/hanny/uiview/TextView.java
@@ -1,38 +1,32 @@
 package com.hanny.uiview;
 
 import android.content.Context;
-import android.graphics.Bitmap;
-import android.graphics.BitmapFactory;
 import android.graphics.Canvas;
 import android.graphics.Paint;
 import android.support.annotation.Nullable;
 import android.util.AttributeSet;
 import android.view.View;
 
-import com.hanny.uiview.R;
-
-public class BitmapView extends View {
+public class TextView extends View {
 
     private Paint paint;
-    private Bitmap bitmap;
 
-    public BitmapView(Context context) {
+    public TextView(Context context) {
         this(context, null);
     }
 
-    public BitmapView(Context context, @Nullable AttributeSet attrs) {
+    public TextView(Context context, @Nullable AttributeSet attrs) {
         this(context, attrs, 0);
     }
 
-    public BitmapView(Context context, @Nullable AttributeSet attrs, int defStyleAttr) {
+    public TextView(Context context, @Nullable AttributeSet attrs, int defStyleAttr) {
         super(context, attrs, defStyleAttr);
         paint = new Paint();
-        bitmap = BitmapFactory.decodeResource(getResources(), R.mipmap.ic_launcher);
     }
 
     @Override
     protected void onDraw(Canvas canvas) {
         super.onDraw(canvas);
-        canvas.drawBitmap(bitmap, 200, 200, paint);
+
     }
 }
