e4b2995ac4b823eae99967ebec256e0dcf030983
HannyYaung <root997@163.com>
2018-07-04 10:45:16 +0800

    自定义view drawBitmap

---
+++ app/src/main/java/com/hanny/uiview/BitmapView.java
@@ -0,0 +1,36 @@
+package com.hanny.uiview;
+
+import android.content.Context;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.Canvas;
+import android.graphics.Paint;
+import android.support.annotation.Nullable;
+import android.util.AttributeSet;
+import android.view.View;
+
+public class BitmapView extends View {
+
+    private Paint paint;
+    private Bitmap bitmap;
+
+    public BitmapView(Context context) {
+        this(context, null);
+    }
+
+    public BitmapView(Context context, @Nullable AttributeSet attrs) {
+        this(context, attrs, 0);
+    }
+
+    public BitmapView(Context context, @Nullable AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+        paint = new Paint();
+        bitmap = BitmapFactory.decodeResource(getResources(), R.mipmap.ic_launcher);
+    }
+
+    @Override
+    protected void onDraw(Canvas canvas) {
+        super.onDraw(canvas);
+        canvas.drawBitmap(bitmap, 200, 200, paint);
+    }
+}
